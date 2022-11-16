f44ce828d5ee678143b19d89f0ae7616ca7de0dc
Roger Montes Ortega <107231881+rogerxm@users.noreply.github.com>
2022-11-16 17:05:44 -0600

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 12;
+export const KEY = '011afdfb-68b6-4a94-9d83-267531b814ff';
+export const MODAL_CLOSE_SEC = 2.5;
